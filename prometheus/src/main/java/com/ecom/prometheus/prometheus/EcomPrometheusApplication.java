7ac542a8c31037b79e7eef759f3b7a4e13a39ce5
deepankar <deepankar.saxena1988@gmail.com>
2026-02-15 01:43:22 +0530

    prometheus tools for observing logs

---
+++ prometheus/src/main/java/com/ecom/prometheus/prometheus/EcomPrometheusApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.prometheus.prometheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomPrometheusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomPrometheusApplication.class, args);
+	}
+
+}
